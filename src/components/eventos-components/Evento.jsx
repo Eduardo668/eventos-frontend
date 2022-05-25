1d03a5a63a69da4260f40b09d14a8ebb89b114be
Eduardo668 <eduardosehn20@gmail.com>
2022-05-25 13:04:05 -0300

    evento

--- src/components/eventos-components/Evento.jsx
+++ src/components/eventos-components/Evento.jsx
@@ -6,7 +6,7 @@ const Evento = ({ evento })=>{
      return (
          <>
             <div className={" relative m-[1.5rem] bg-dark w-[23rem] rounded-[30px] h-[30rem]  "}>
-               <img className="w-full h-full bg-cover rounded-[30px] opacity-80 relative" src={evento.fotoEvento} alt="" />
+               <img className="w-full h-full bg-cover rounded-[30px] opacity-80 relative" src={evento.fotoLink} alt="" />
                <div className="relative bottom-[10rem]">
                <h3 className=" p-3 w-full rounded-t-[30px] bg-gradient-to-r  from-indigo-500 to-purple-900 text-white font-bold z-10 text-[1.5rem] relative bottom-[20rem] mb-4">{evento.nome_evento}</h3>
                 <h3 className="p-2 w-[20rem] bg-gradient-to-r  from-indigo-500 to-purple-900 rounded-r-[30px] text-white font-bold z-10 text-[1.5rem] relative bottom-[20rem] mb-4 ">Local - {evento.local}</h3>
